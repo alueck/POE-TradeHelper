b7333af441be32170a44aca97266533188eda9f2
Adrian Lück <adrian-lueck@gmx.de>
2020-02-09 12:44:40 +0100

    Catch exceptions in ItemSearchResultOverlayViewModel for prototyping

--- Source/POETradeHelper.ItemSearch/ViewModels/ItemSearchResultOverlayViewModel.cs
+++ Source/POETradeHelper.ItemSearch/ViewModels/ItemSearchResultOverlayViewModel.cs
@@ -1,8 +1,10 @@
-﻿using POETradeHelper.ItemSearch.Contract.Models;
+﻿using POETradeHelper.Common.UI.Models;
+using POETradeHelper.ItemSearch.Contract.Models;
 using POETradeHelper.ItemSearch.Contract.Services;
 using POETradeHelper.PathOfExileTradeApi.Models;
 using POETradeHelper.PathOfExileTradeApi.Services;
 using ReactiveUI;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -20,22 +22,42 @@ namespace POETradeHelper.ItemSearch.ViewModels
         }
 
         private ItemListingsQueryResult itemListing;
+        private Message message;
 
         public ItemListingsQueryResult ItemListings
         {
-            get => itemListing;
+            get => this.itemListing;
             set => this.RaiseAndSetIfChanged(ref itemListing, value);
+        }
+
+        public Message Message
+        {
+            get => this.message;
+            set => this.RaiseAndSetIfChanged(ref message, value);
         }
 
         public async Task SetListingForItemUnderCursorAsync(CancellationToken cancellationToken = default)
         {
-            Item item = await searchItemProvider.GetItemFromUnderCursorAsync(cancellationToken);
-
-            ItemListingsQueryResult itemListing = await this.poeTradeApiClient.GetListingsAsync(item, cancellationToken);
-
-            if (itemListing != null && !cancellationToken.IsCancellationRequested)
+            try
             {
-                this.ItemListings = itemListing;
+                this.Message = null;
+
+                Item item = await searchItemProvider.GetItemFromUnderCursorAsync(cancellationToken);
+
+                ItemListingsQueryResult itemListing = await this.poeTradeApiClient.GetListingsAsync(item, cancellationToken);
+
+                if (itemListing != null && !cancellationToken.IsCancellationRequested)
+                {
+                    this.ItemListings = itemListing;
+                }
+            }
+            catch (Exception ex)
+            {
+                this.Message = new Message
+                {
+                    Text = ex.ToString(),
+                    Type = MessageType.Error
+                };
             }
         }
     }
