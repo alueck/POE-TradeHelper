7b821d33f5e7486740d24de533deee64bd362410
Adrian Lück <adrian-lueck@gmx.de>
2020-03-03 14:29:05 +0100

    Fix data class for StatsDataService

--- Source/POETradeHelper.PathOfExileTradeApi/Models/Data/Data.cs
+++ Source/POETradeHelper.PathOfExileTradeApi/Models/Data/Data.cs
@@ -4,7 +4,15 @@ namespace POETradeHelper.PathOfExileTradeApi.Models
 {
     public class Data<TType>
     {
-        public string Id { get; set; }
+        private string id;
+
+        public string Id
+        {
+            get => this.id ?? this.Label;
+            set => this.id = value;
+        }
+
+        public string Label { get; set; }
 
         public IList<TType> Entries { get; set; }
     }
