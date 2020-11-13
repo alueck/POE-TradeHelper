9df1c4ec700cacadd506473e999019df7c5007fd
Adrian Lück <adrian-lueck@gmx.de>
2020-11-13 19:18:08 +0100

    Update projects to .NET 5.0 (#25)
    * Update projects to .NET 5.0
    
    * Update NuGet packages
    
    * Decouple application bootstrapper more from modules
    
    * Update workflow
    
    * Remove appsettings.json from output
    
    * Fix Create release workflow

---
+++ Source/POETradeHelper.Win32/Win32Module.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using POETradeHelper.Common.Contract;
+using WindowsHook;
+
+namespace POETradeHelper.Win32
+{
+    internal class Win32Module : IModule
+    {
+        public void RegisterServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton(Hook.GlobalEvents());
+        }
+    }
+}
