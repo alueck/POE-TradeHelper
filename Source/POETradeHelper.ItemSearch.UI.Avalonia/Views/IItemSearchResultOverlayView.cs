4c653a617a69e71318b62f78f6113ee9505e1d37
Adrian Lück <github@sl.alueck.de>
2026-08-08 09:42:46 +0200

    Fix overlay visibility status not being tracked correctly (#78)

--- Source/POETradeHelper.ItemSearch.UI.Avalonia/Views/IItemSearchResultOverlayView.cs
+++ Source/POETradeHelper.ItemSearch.UI.Avalonia/Views/IItemSearchResultOverlayView.cs
@@ -1,10 +1,12 @@
-﻿using Avalonia;
+﻿using System.ComponentModel;
+
+using Avalonia;
 
 using POETradeHelper.Common.UI;
 
 namespace POETradeHelper.ItemSearch.UI.Avalonia.Views
 {
-    public interface IItemSearchResultOverlayView : IHideable, IDataContextProvider
+    public interface IItemSearchResultOverlayView : IHideable, IDataContextProvider, INotifyPropertyChanged
     {
         bool IsVisible { get; set; }
     }
